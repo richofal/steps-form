a995473e78587673b8a590deba167f44cfd48fff
Richo Febrian <richofal25@gmail.com>
2024-12-29 18:00:13 +0700

    Membuat UI jadi dinamis dengan state dan event

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
+
 function App() {
+  const [step, setStep] = useState(1);
+
   function handleNext() {
-    alert("Next button clicked");
+    if (step < 3) {
+      setStep(step + 1);
+      console.log(step);
+    }
+  }
+  function handlePrev() {
+    if (step > 1) {
+      setStep(step - 1);
+      console.log(step);
+    }
   }
 
   return (
     <>
       <div className="w-[600px] bg-[#f7f7f7] rounded-[7px] py-[25px] px-[100px] my-[100px] mx-auto">
         <div className="flex justify-between">
-          <div className="h-10 aspect-square bg-[#e7e7e7] rounded-[50%] flex items-center justify-center text-[18px]">
+          <div
+            className={`h-10 aspect-square bg-[#e7e7e7] rounded-[50%] flex items-center justify-center text-[18px] ${
+              step >= 1 ? "bg-[#526d82] text-[#fff]" : ""
+            }`}
+          >
             1
           </div>
-          <div className="h-10 aspect-square bg-[#e7e7e7] rounded-[50%] flex items-center justify-center text-[18px]">
+          <div
+            className={`h-10 aspect-square bg-[#e7e7e7] rounded-[50%] flex items-center justify-center text-[18px] ${
+              step >= 2 ? "bg-[#526d82] text-[#fff]" : ""
+            }`}
+          >
             2
           </div>
-          <div className="h-10 aspect-square bg-[#e7e7e7] rounded-[50%] flex items-center justify-center text-[18px]">
+          <div
+            className={`h-10 aspect-square bg-[#e7e7e7] rounded-[50%] flex items-center justify-center text-[18px] ${
+              step >= 3 ? "bg-[#526d82] text-[#fff]" : ""
+            }`}
+          >
             3
           </div>
         </div>
@@ -23,7 +48,7 @@ function App() {
         <div className="flex justify-between">
           <button
             className="bg-[#526D82] text-white border-none py-2.5 px-[15px] rounded-[100px] text-[14px font-bold] flex items-center gap-2.5"
-            onMouseEnter={() => alert("Prev button hovered")}
+            onClick={handlePrev}
           >
             Prev
           </button>
